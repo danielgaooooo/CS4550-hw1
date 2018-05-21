2b0382b82fb2b54642ea5b290201d7ad75d267e1
Daniel Gao <danielgaooooo@gmail.com>
2018-05-21 19:31:50 -0400

    done with serverside

--- src/main/java/com/example/myapp/services/LessonService.java
+++ src/main/java/com/example/myapp/services/LessonService.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -66,12 +67,29 @@ public class LessonService {
 	}
 	
 	@GetMapping("/api/course/{cid}/module/{mid}/lesson")
-	public List<Lesson> findAllLessonsForModule(
-			@PathVariable("mid") int modId) {
+	public List<Lesson> findAllLessonsForModule(@PathVariable("mid") int modId) {
 		Optional<Module> mod = moduleRepository.findById(modId);
 		if (mod.isPresent()) {
 			Module m = mod.get();
 			return m.getLessons();
+		} else {
+			return null;
+		}
+	}
+	@PutMapping("api/lesson/{lessonId}")
+	public Lesson updateLesson(@PathVariable("lessonId") int lessonId, 
+			@RequestBody Lesson newLesson) {
+		Optional<Lesson> data = lessonRepository.findById(lessonId);
+		if (data.isPresent()) {
+			Lesson lesson = data.get();
+			if (newLesson.getTitle() != null) {
+				lesson.setTitle(newLesson.getTitle());
+			}
+			if (newLesson.getModule() != null) {
+				lesson.setModule(newLesson.getModule());
+			}
+			lessonRepository.save(lesson);
+			return lesson;
 		} else {
 			return null;
 		}
--- src/main/java/com/example/myapp/services/ModuleService.java
+++ src/main/java/com/example/myapp/services/ModuleService.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,7 +28,7 @@ public class ModuleService {
 
 	@Autowired
 	ModuleRepository moduleRepository;
-	
+
 	@PostMapping("/api/course/{courseId}/module")
 	public Module createModule(
 			@PathVariable("courseId") int courseId,
@@ -41,10 +42,9 @@ public class ModuleService {
 		}
 		return null;		
 	}
-	
+
 	@GetMapping("/api/course/{courseId}/module")
-	public List<Module> findAllModulesForCourse(
-			@PathVariable("courseId") int courseId) {
+	public List<Module> findAllModulesForCourse(@PathVariable("courseId") int courseId) {
 		Optional<Course> data = courseRepository.findById(courseId);
 		if(data.isPresent()) {
 			Course course = data.get();
@@ -52,16 +52,45 @@ public class ModuleService {
 		}
 		return null;		
 	}
-	
+
 	@DeleteMapping("/api/module/{moduleId}")
-	public void deleteModule(@PathVariable("moduleId") int moduleId)
-	{
+	public void deleteModule(@PathVariable("moduleId") int moduleId) {
 		moduleRepository.deleteById(moduleId);
 	}
-	
+
 	@GetMapping("/api/module")
-	public List<Module> findAllModules()
-	{
+	public List<Module> findAllModules() {
 		return (List<Module>) moduleRepository.findAll();
+	}
+
+	@GetMapping("/api/module/{moduleId}")
+	public Module findModuleById(@PathVariable("moduleId") int id) {
+		Optional<Module> m = moduleRepository.findById(id);
+		if (m.isPresent()) {
+			return m.get();
+		} else {
+			return null;
+		}
+	}
+
+	@PutMapping("api/module/{moduleId}")
+	public Module updateModule(@PathVariable("moduleId") int moduleId, @RequestBody Module newMod) {
+		Optional<Module> data = moduleRepository.findById(moduleId);
+		if (data.isPresent()) {
+			Module module = data.get();
+			if (newMod.getCourse() != null) {
+				module.setCourse(newMod.getCourse());
+			}
+			if (newMod.getLessons() != null) {
+				module.setLessons(newMod.getLessons());
+			}
+			if (newMod.getTitle() != null) {
+				module.setTitle(newMod.getTitle());
+			}
+			moduleRepository.save(module);
+			return module;
+		} else {
+			return null;
+		}
 	}
 }
